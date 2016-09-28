cf7310450e69677ea3f4c8cb491e1e07079c276e
Chris Badahdah <a.s.badahdah@gmail.com>
2016-09-28 07:54:45 -0700

    Removed number of allocs.

--- src/vm.c
+++ src/vm.c
@@ -3,9 +3,10 @@
 typedef struct alloc_t alloc_t;
 
 struct alloc_t {
-  object_t *o;
   alloc_t *next;
 };
+
+#define alloc_data(a) (object_t*)(((char*) a) + sizeof(alloc_t))
 
 struct vm_t {
   alloc_t *root_alloc;
@@ -13,8 +14,7 @@ struct vm_t {
 };
 
 static alloc_t *make_alloc(size_t n) {
-  alloc_t *alloc = (alloc_t*) malloc(sizeof(alloc_t));
-  alloc->o = (object_t*) malloc(n);
+  alloc_t *alloc = (alloc_t*) malloc(sizeof(alloc_t) + n);
   alloc->next = NULL;
   return alloc;
 }
@@ -46,14 +46,14 @@ static void mark(object_t *o) {
 static void sweep(alloc_t **root) {
   alloc_t **prev = root, *alloc = *root;
   while (alloc != NULL) {
-    if (alloc->o == NULL || alloc->o->marked == 0) {
-      free_object(alloc->o);
+    object_t *o = alloc_data(alloc);
+    if (o == NULL || o->marked == 0) {
       *prev = alloc->next;
       alloc_t *tmp = alloc;
       alloc = alloc->next;
       free(tmp);
     } else {
-      alloc->o->marked = 0;
+      o->marked = 0;
       if (alloc->next != NULL) {
         prev = &alloc->next;
       }
@@ -87,7 +87,6 @@ void vm_set_root_env(vm_t *vm, object_t *env) {
 object_t *vm_alloc(vm_t *vm, size_t n) {
   alloc_t *alloc = make_alloc(n);
   alloc->next = vm->root_alloc;
-  vm->root_alloc = alloc;
-  return alloc->o;
+  return alloc_data(alloc);
 }
 
